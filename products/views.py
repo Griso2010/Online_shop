5426c0bb5c6b87a3d5e9644bbe014f5a0d8e2c9c
Grigoriy <sokolov.g.2016@gmail.com>
2023-06-01 19:46:04 +0300

    Fixed index.html. directory media. Added urls for images

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from products.models import ProductCategory, Product
 
 def index(request):
     context = {'title': 'Store'}
     return render(request, 'products/index.html', context)
 
 def products(request):
-    context = {'title': 'Store - каталог'}
+    context = {
+        'title': 'Store - каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
     return render(request, 'products/products.html', context)
