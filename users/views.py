f1cd38278bacfc4f74fbefefc55423610db3ab80
Grigoriy <sokolov.g.2016@gmail.com>
2023-06-03 18:38:16 +0300

    Added login for user. Added file forms and UserLoginForm. Fixed login.html

--- users/views.py
+++ users/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+from django.contrib import auth
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context)
 
 def register(request):
     return render(request, 'users/register.html')
