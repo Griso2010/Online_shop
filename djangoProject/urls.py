5202bb351ef4c33988f4cbf601da4ac925fe6037
Grigoriy <sokolov.g.2016@gmail.com>
2023-06-03 16:16:45 +0300

    Created app users. Added templates for login/registration. Created model users.

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products.views import index, products
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,7 +7,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include(('products.urls', 'products'))),
+    path('users/', include(('users.urls', 'users'))),
+
 ]
 
 if settings.DEBUG:
