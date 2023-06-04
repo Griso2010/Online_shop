fde5373e4410a8524324a862a7cd67bef5a3772c
Grigoriy <sokolov.g.2016@gmail.com>
2023-06-04 06:21:24 +0300

    Added logout and profile view. Fixed registration/login/profile/base template.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from users.forms import UserLoginForm, UserRegistrationForm
-from django.contrib import auth
+from users.forms import *
+from django.contrib import auth, messages
 from django.urls import reverse
+
+__all_ = (
+    'login',
+    'registration',
+    'profile',
+    'logout',
+)
 
 
 def login(request):
@@ -14,10 +21,11 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                return redirect(reverse('index'))
+                messages.success(request, 'Вход был произведен успешно')
+                return HttpResponseRedirect(reverse('index'))
     else:
         form = UserLoginForm()
-    context = {'form': UserLoginForm()}
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 def registration(request):
@@ -25,10 +33,28 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('users:login'))
+            messages.success(request, 'Поздравляем! вы успешно зарегистрированы')
+            return redirect(reverse('users:login'))
 
     else:
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
 
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профиль успешно обновлен')
+            return redirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('index'))
