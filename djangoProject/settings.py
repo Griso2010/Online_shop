341321e3d9478648c711c2501d0ce5e2fb138670
Grigoriy <sokolov.g.2016@gmail.com>
2023-07-05 10:49:58 +0300

    Changed DB from Sqlite to Postgres

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store_db',
+        'USER': 'store_username',
+        'PASSWORD': 'store_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
